0504849159566b58bfa1e92a16d10d6743760931
jaybojaybojaybo <jasun.feddema@gmail.com>
2017-03-07 23:52:00 -0800

    add word length requirement to output array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-// function isLongEnough(value) {
-//   return value >= 3;
-// }
+
 
 $(document).ready(function() {
 
@@ -10,10 +8,12 @@ $(document).ready(function() {
     var sentence = $("#userSentence").val();
     var splitSen = sentence.split(" ");
     var lengths = splitSen.map(function(word) {
-      return word.length;
-    })
-
-    // var newSen = splitSen.filter(isLongEnough);
+      if (word.length > 3) {
+        return word
+      }; 
+      });
+    console.log(sentence);
+    console.log(lengths);
     $("#outputs").text(lengths);
   });
 });
